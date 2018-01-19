b6c960166d6e516234e7bb6a738ed0a9b9992b38
Emily <33560517+emiliska@users.noreply.github.com>
2018-01-19 15:05:42 -0500

    Create setupUserPrivs.sh
    Creates a new user, adds user to sudo, prints /etc/shadow & /etc/passwd to console.

---
+++ setupUserPrivs.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+ 	echo "!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!"
+adduser emily
+	echo "STEP 1 OF 4"
+	echo "adduser: emily"
+	echo ""
+gpasswd -a emily wheel
+	echo "STEP 2 OF 4"
+	echo "adduser to sudo"
+	echo ""
+	echo "!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!"
+	echo ""
+	echo "STEP 3 OF 4"
+cat /etc/passwd
+	echo ""
+	echo ""
+	echo "STEP 4 OF 4"
+cat /etc/shadow
+	echo ""
+	echo "!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!"
+	echo "END OF SCRIPT: CARRY ON"
